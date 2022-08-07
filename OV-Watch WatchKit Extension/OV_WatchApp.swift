655546271c8230944b2295fe235786aac103d602
Peter Harry <Peter.harry56@gmail.com>
2022-08-08 08:43:56 +1000

    Initial Commit

---
+++ OV-Watch WatchKit Extension/OV_WatchApp.swift
@@ -0,0 +1,19 @@
+//
+//  OV_WatchApp.swift
+//  OV-Watch WatchKit Extension
+//
+//  Created by Peter Harry on 8/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct OV_WatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
